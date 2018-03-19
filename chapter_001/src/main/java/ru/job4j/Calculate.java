8d45d7ab824db868e0e876972517d8725512ddb5
DmitriyKostyanetsky <onlywarinfarfuture@gmail.com>
2018-03-19 21:54:55 +0300

    1.3 JavDoc. Code convetion.

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,16 @@
 package ru.job4j;
 
+ /**
+  *Calculate.
+  *
+  *@author Dmitriy Kostyanetsky (onlywarinfarfuture@gmail.com)
+  */
 public class Calculate {
 	
+	/**
+	*Конструктор, вывод строки в консоль.
+	*@param args - args.
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+*Package for calculate task.
+*
+*@author Dmitriy Kostyanetsky (onlywarinfarfuture@gmail.com)
+*@version 1
+*@since 19.03.18
+*/
+
+package ru.job4j;
