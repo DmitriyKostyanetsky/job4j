0612332521ef6fd6aa86cf9f4e92a5672759c554
DmitriyKostyanetsky <onlywarinfarfuture@gmail.com>
2018-03-26 00:19:28 +0300

    4.2. Создать программу вычисляющую факториал.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+/**
+ *@author Dmitriy Kostyanetsky (onlywarinfarfuture@gmail.com)
+ *@version 1
+ *@since 26.03.18
+ */
+public class Factorial {
+
+    /**
+     * Рассчет факториала
+     * @param n положительное целое число
+     * @return Фактоиал
+     */
+    public int calc(int n) {
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            if (n == 0) {
+                return factorial;
+            } else {
+                factorial = factorial * i;
+            }
+        }
+        return factorial;
+    }
+}
