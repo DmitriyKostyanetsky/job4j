0e25379fa85c8b88279af0aeac5a58887d293342
DmitriyKostyanetsky <onlywarinfarfuture@gmail.com>
2018-03-26 14:03:37 +0300

    4.2. Программа вычисляющая факториал(дополнение).

--- chapter_001/src/main/java/ru/job4j/loop/BoardTest.java
+++ chapter_001/src/main/java/ru/job4j/loop/BoardTest.java
@@ -8,11 +8,18 @@ public class BoardTest {
     @Test
     public void when3x3() {
         Board board = new Board();
-        String rsl = board.paint(3, 3);
-        String ln = System.lineSeparator();
-        assertThat(rsl, is(
-                String.format("X X%s X %sX X%s", ln, ln, ln)
-                )
-        );
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("X X%s X %sX X%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void when5x4() {
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("X X X%s X X %sX X X%s X X %s", line, line, line, line);
+        assertThat(result, is(expected));
     }
 }
