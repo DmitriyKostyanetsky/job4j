8a824a820a3d723a06cc18a7c57eccbf84f4545d
DmitriyKostyanetsky <onlywarinfarfuture@gmail.com>
2018-05-08 16:01:04 +0300

    Кофемашина(исправление)

--- chapter_002/src/main/java/ru/job4j/Coffee.java
+++ chapter_002/src/main/java/ru/job4j/Coffee.java
@@ -1,10 +1,12 @@
 package ru.job4j;
 
+import java.util.ArrayList;
+
 public class Coffee {
     /**
      * Номиналы монет в автомате
      */
-    private static final int[] NOTE = new int[] {1, 2, 5, 10};
+    private static final int[] NOTE = new int[]{10, 5, 2, 1};
 
     /**
      * Метод выдачи сдачи автомата
@@ -12,42 +14,15 @@ public class Coffee {
      * @param price цена кофе
      * @return возвращает сдачу
      */
-    int[] changes(int value, int price) {
+    public ArrayList<Integer> changes(int value, int price) {
         int remainder = value - price;
-        int[] result = new int[15];
-
-        for (int i = 0; remainder != 0; i++) {
-            if (remainder >= 10) {
-                remainder = remainder - NOTE[3];
-                result[i] = NOTE[3];
-                continue;
-            }
-            if (remainder >= 5) {
-                remainder = remainder - NOTE[2];
-                result[i] = NOTE[2];
-                continue;
-            }
-            if (remainder >= 2) {
-                remainder = remainder - NOTE[1];
-                result[i] = NOTE[1];
-                continue;
-            }
-            if (remainder < 2 && remainder != 0) {
-                remainder = remainder - NOTE[0];
-                result[i] = NOTE[0];
-                continue;
+        ArrayList<Integer> result = new ArrayList<>();
+        for (int coin : NOTE) {
+            while (remainder - coin >= 0) {
+                result.add(coin);
+                remainder = remainder - coin;
             }
         }
-
-        int j = 0;
-        for (int i = 0; i < result.length; i++) {
-            if (result[i] != 0) {
-                result[j++] = result[i];
-            }
-        }
-        int[] arrayWithoutNull = new int[j];
-        System.arraycopy(result, 0, arrayWithoutNull, 0, j);
-
-        return arrayWithoutNull;
+        return result;
     }
 }
--- chapter_002/src/test/java/ru/job4j/CoffeeTest.java
+++ chapter_002/src/test/java/ru/job4j/CoffeeTest.java
@@ -1,7 +1,9 @@
 package ru.job4j;
 
 import org.junit.Test;
-
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -9,24 +11,28 @@ public class CoffeeTest {
     @Test
     public void whenValue100AndPrice35ThenSurrender65() {
         Coffee coffee = new Coffee();
-        int[] result = coffee.changes(100, 35);
-        int[] except = new int[] {10, 10, 10, 10, 10, 10, 5};
+        ArrayList<Integer> result = coffee.changes(100, 35);
+        List<Integer> except = Arrays.asList(
+                10, 10, 10, 10, 10, 10, 5
+        );
         assertThat(result, is(except));
     }
 
     @Test
     public void whenValue46AndPrice45ThenSurrender1() {
         Coffee coffee = new Coffee();
-        int[] result = coffee.changes(46, 45);
-        int[] except = new int[] {1};
+        ArrayList<Integer> result = coffee.changes(46, 45);
+        List<Integer> except = Arrays.asList(1);
         assertThat(result, is(except));
     }
 
     @Test
     public void whenValue100AndPrice61ThenSurrender39() {
         Coffee coffee = new Coffee();
-        int[] result = coffee.changes(100, 61);
-        int[] except = new int[] {10, 10, 10, 5, 2, 2};
+        ArrayList<Integer> result = coffee.changes(100, 61);
+        List<Integer> except = Arrays.asList(
+                10, 10, 10, 5, 2, 2
+        );
         assertThat(result, is(except));
     }
 }
