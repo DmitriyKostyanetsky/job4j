e5f6a1143c9b3710209f77d962d5f178f7ed9761
DmitriyKostyanetsky <onlywarinfarfuture@gmail.com>
2018-06-18 12:21:45 +0300

    2. Не перекрывать equals hashCode

---
+++ chapter_004/src/test/java/ru/job4j/map/UserTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserTest {
+
+    private User user1;
+    private User user2;
+    private Map<User, Object> map;
+
+    @Before
+    public void setUp() {
+        user1 = new User("Ivan", 1, new GregorianCalendar(1990, Calendar.DECEMBER, 30));
+        user2 = new User("Ivan", 1, new GregorianCalendar(1990, Calendar.DECEMBER, 30));
+        map = new HashMap<>();
+    }
+
+    @Test
+    public void whenPutEqualsObjectsThen() {
+        map.put(user1, user1);
+        map.put(user2, user2);
+        System.out.println(map);
+    }
+}
