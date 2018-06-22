14392f44c3b064089de1ad4a4bc15dc462593e44
DmitriyKostyanetsky <onlywarinfarfuture@gmail.com>
2018-06-22 12:14:20 +0300

    3. Реализовать коллекцию типа Set на базе хэш-таблицы

--- chapter_004/src/main/java/ru/job4j/set/SimpleHashSet.java
+++ chapter_004/src/main/java/ru/job4j/set/SimpleHashSet.java
@@ -13,34 +13,25 @@ public class SimpleHashSet<E> {
 
     private void checkArray() {
         if (position == container.length) {
+            E[] temp = container;
             container = Arrays.copyOf(container, container.length + 10);
-            for (int i = 0; i < container.length; i++) {
-                if (container[i] != null) {
-                    int newIndex = Math.abs(container[i].hashCode() % container.length);
-                    if (container[newIndex] == null) {
-                        container[newIndex] = container[i];
-                        container[i] = null;
-                        continue;
-                    }
-                    if (container[newIndex] != null) {
-                        E temp = container[newIndex];
-                        container[newIndex] = container[i];
-                        container[i] = temp;
-                    }
-                }
+            for (int i = 0; i < temp.length; i++) {
+                int newIndex = Math.abs(temp[i].hashCode() % container.length);
+                container[i] = null;
+                container[newIndex] = temp[i];
             }
         }
     }
 
     private boolean hasDuplicate(E e) {
         int hashSum = Math.abs(e.hashCode() % container.length);
-        return container[hashSum] == null;
+        return container[hashSum] != null;
     }
 
     boolean add(E e) {
         checkArray();
         int hashSum = Math.abs(e.hashCode() % container.length);
-        if (hasDuplicate(e)) {
+        if (!hasDuplicate(e)) {
             container[hashSum] = e;
             position++;
             return true;
