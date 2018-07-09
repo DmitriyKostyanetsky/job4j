9e37b61fc0ea01b9e7659b42cb85f9cfb8200bd2
DmitriyKostyanetsky <onlywarinfarfuture@gmail.com>
2018-07-09 12:24:43 +0300

    Статистика по коллекции

--- chapter_004/src/main/java/ru/job4j/task/Store.java
+++ chapter_004/src/main/java/ru/job4j/task/Store.java
@@ -1,12 +1,46 @@
 package ru.job4j.task;
 
-import ru.job4j.list.DynamicList;
+import java.util.Iterator;
+import java.util.List;
 
 public class Store {
+    private Info info = new Info();
 
-    public void infoDiff(DynamicList<User> previous, DynamicList<User> current) {
-        System.out.println("Previous: Add: " + previous.getAddCount() + " Change: " + previous.getChangeCount() + " Remove: " + previous.getRemCount());
-        System.out.println("Current: Add: " + current.getAddCount() + " Change: " + current.getChangeCount() + " Remove: " + current.getRemCount());
+    public Info diff(List<User> current, List<User> next) {
+        info = info.changed(current, next);
+        System.out.println("Added: " + info.addCount + " Changed: " + info.chCount + " Deleted: " + info.remCount);
+        return info;
+    }
+
+    private class Info {
+        private Iterator<User> curIterator;
+        private Iterator<User> nextIterator;
+        private int addCount = 0;
+        private int chCount = 0;
+        private int remCount = 0;
+
+        private Info() {}
+
+        private Info(int addCount, int chCount, int remCount) {
+            this.addCount = addCount;
+            this.chCount = chCount;
+            this.remCount = remCount;
+        }
+
+        private Info changed(List<User> current, List<User> next) {
+            curIterator = current.iterator();
+            nextIterator = next.iterator();
+            while (curIterator.hasNext() && nextIterator.hasNext()) {
+                if (!curIterator.next().equals(nextIterator.next())) {
+                    chCount++;
+                }
+            }
+            addCount = next.size() - current.size();
+            if (addCount < 0) {
+                remCount = Math.abs(addCount);
+            }
+            return new Info(addCount, chCount, remCount);
+        }
     }
 
     static class User {
--- chapter_004/src/test/java/ru/job4j/task/StoreTest.java
+++ chapter_004/src/test/java/ru/job4j/task/StoreTest.java
@@ -1,23 +1,31 @@
 package ru.job4j.task;
 
+import org.junit.Before;
 import org.junit.Test;
-import ru.job4j.list.DynamicList;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StoreTest {
 
-    private DynamicList<Store.User> previous;
-    private DynamicList<Store.User> current;
+    private List<Store.User> current;
+
+    @Before
+    public void setUp() {
+        current = new ArrayList<>();
+        current.add(new Store.User(1, "Ivan"));
+        current.add(new Store.User(2, "Maxim"));
+        current.add(new Store.User(3, "Alexey"));
+    }
 
     @Test
     public void checkInfo() {
+        List<Store.User> next = new ArrayList<>();
+        next.addAll(current);
         Store store = new Store();
-        Store.User[] users = new Store.User[3];
-        previous = new DynamicList<>(users);
-        current = previous;
-        previous.add(new Store.User(1, "Ivan"));
-        previous.add(new Store.User(2, "Maxim"));
-        previous.add(new Store.User(3, "Alexey"));
-        current.remove(2);
-        store.infoDiff(previous, current);
+        next.remove(2);
+        next.add(new Store.User(3, "Jim"));
+        next.add(new Store.User(4, "Martin"));
+        store.diff(current, next);
     }
 }
