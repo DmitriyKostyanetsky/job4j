2a40395dee70777b42a8b8f420afc96e31de82eb
DmitriyKostyanetsky <onlywarinfarfuture@gmail.com>
2018-07-11 12:24:31 +0300

    Статистику по коллекции

--- chapter_004/src/main/java/ru/job4j/task/Store.java
+++ chapter_004/src/main/java/ru/job4j/task/Store.java
@@ -1,20 +1,17 @@
 package ru.job4j.task;
 
-import java.util.Iterator;
 import java.util.List;
 
 public class Store {
     private Info info = new Info();
 
     public Info diff(List<User> current, List<User> next) {
-        info = info.changed(current, next);
+        info = info.deleted(current, next);
         System.out.println("Added: " + info.addCount + " Changed: " + info.chCount + " Deleted: " + info.remCount);
         return info;
     }
 
     private class Info {
-        private Iterator<User> curIterator;
-        private Iterator<User> nextIterator;
         private int addCount = 0;
         private int chCount = 0;
         private int remCount = 0;
@@ -27,18 +24,21 @@ public class Store {
             this.remCount = remCount;
         }
 
-        private Info changed(List<User> current, List<User> next) {
-            curIterator = current.iterator();
-            nextIterator = next.iterator();
-            while (curIterator.hasNext() && nextIterator.hasNext()) {
-                if (!curIterator.next().equals(nextIterator.next())) {
-                    chCount++;
+        private Info deleted(List<User> current, List<User> next) {
+            for (int i = 0; i < current.size(); i++) {
+                for (int j = 0; j < next.size(); j++) {
+                    if (current.get(i).equals(next.get(j))) {
+                        if (!current.get(i).name.equals(next.get(j).name)) {
+                            chCount++;
+                        }
+                        break;
+                    }
+                    if (!current.get(i).equals(next.get(j)) && j + 1 == next.size()) {
+                        remCount++;
+                    }
                 }
             }
-            addCount = next.size() - current.size();
-            if (addCount < 0) {
-                remCount = Math.abs(addCount);
-            }
+            addCount = Math.abs(current.size() - next.size()) + remCount;
             return new Info(addCount, chCount, remCount);
         }
     }
@@ -50,6 +50,18 @@ public class Store {
         public User(int id, String name) {
             this.id = id;
             this.name = name;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) {
+                return true;
+            }
+            if (!(obj instanceof User)) {
+                return false;
+            }
+            User user = (User) obj;
+            return Integer.compare(id, user.id) == 0;
         }
     }
 }
--- chapter_004/src/test/java/ru/job4j/task/StoreTest.java
+++ chapter_004/src/test/java/ru/job4j/task/StoreTest.java
@@ -9,23 +9,23 @@ import java.util.List;
 public class StoreTest {
 
     private List<Store.User> current;
+    private List<Store.User> next;
 
     @Before
     public void setUp() {
         current = new ArrayList<>();
+        next = new ArrayList<>();
         current.add(new Store.User(1, "Ivan"));
         current.add(new Store.User(2, "Maxim"));
         current.add(new Store.User(3, "Alexey"));
+        next.add(new Store.User(2, "Maxim"));
+        next.add(new Store.User(3, "Andrey"));
+        next.add(new Store.User(4, "Stanislav"));
     }
 
     @Test
     public void checkInfo() {
-        List<Store.User> next = new ArrayList<>();
-        next.addAll(current);
         Store store = new Store();
-        next.remove(2);
-        next.add(new Store.User(3, "Jim"));
-        next.add(new Store.User(4, "Martin"));
         store.diff(current, next);
     }
 }
