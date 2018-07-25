2dc5902411748e393f9b392824a8b483964e95a2
DmitriyKostyanetsky <onlywarinfarfuture@gmail.com>
2018-07-25 13:48:31 +0300

    Exceptions

--- other/src/main/java/exception/ExceptionsExample.java
+++ other/src/main/java/exception/ExceptionsExample.java
@@ -2,31 +2,21 @@ package exception;
 
 public class ExceptionsExample {
 
-    private int[] array;
-    private int number;
-    private TestForExceptions test;
-
-    public ExceptionsExample(int[] array, int number) {
-        this.array = array;
-        this.number = number;
-        test = new TestForExceptions();
-    }
-
-    public int catchExceptions(int[] array, int number) throws Exception {
-        int result = 0;
-        try {
-            result = array[1] / number;
-        } catch (ArrayIndexOutOfBoundsException e) {
-            test.myFunc(1);
-        } catch (IndexOutOfBoundsException e) {
-            test.myFunc(2);
-        }catch (ArithmeticException e) {
-            test.myFunc(0);
-        } catch (RuntimeException e) {
-            System.out.println("RuntimeException handling!");
-        } catch (Exception e) {
+    public void catchExceptions(int parameter) throws Exception {
+        if (parameter == 1) {
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        if (parameter == 2) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (parameter == 3) {
+            throw new ArithmeticException();
+        }
+        if (parameter == 4) {
+            throw new RuntimeException();
+        }
+        if (parameter == 5) {
             throw new Exception();
         }
-        return result;
     }
 }
--- other/src/test/java/exception/ExceptionExampleTest.java
+++ other/src/test/java/exception/ExceptionExampleTest.java
@@ -2,39 +2,37 @@ package exception;
 
 import org.junit.Test;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-
 public class ExceptionExampleTest {
 
     private ExceptionsExample example;
-    private int[] array;
-    private int number;
 
     @Test
-    public void whenDivideByZeroThenArithmeticException() throws Exception {
-        array = new int[] {1, 2};
-        number = 0;
-        example = new ExceptionsExample(array, number);
-        int result = example.catchExceptions(array, number);
-        assertEquals(result, 0);
+    public void testException1()  {
+        example = new ExceptionsExample();
+        try {
+            example.catchExceptions(1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void whenDivideByNothingThenOutOfBounds() throws Exception {
-        array = new int[] {1};
-        number = 1;
-        example = new ExceptionsExample(array, number);
-        int result = example.catchExceptions(array, number);
-        assertNotEquals(result, 1);
+    public void testException2()  {
+        example = new ExceptionsExample();
+        try {
+            example.catchExceptions(3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
-    public void whenDivideBy1Then2() throws Exception {
-        array = new int[] {1, 2};
-        number = 1;
-        example = new ExceptionsExample(array, number);
-        int result = example.catchExceptions(array, number);
-        assertThat(result, is(2));
+    public void testException3()  {
+        example = new ExceptionsExample();
+        try {
+            example.catchExceptions(4);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
